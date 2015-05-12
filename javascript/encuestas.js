eedbe5f5ace7ae0ca37875da3d3a5df32a9825c3
luisarturo1989@gmail.com <luisarturo1989@gmail.com>
2015-05-12 11:02:16 -0500

    refresh listas
    Se refresta el listado

--- javascript/encuestas.js
+++ javascript/encuestas.js
@@ -1,6 +1,23 @@
 $(function(){
-	alert("Hola amigos");	
-    $( "#clasificacion" ).sortable();
-    $( "#clasificacion" ).disableSelection();
+	alert("Hola amigos");
+	$("#clasificacion").sortable({
+		beforeStop: function( event, ui ) {
+			cambiarNumeros();
+		}
+	});
+
+    $("#clasificacion").disableSelection();
     
+
+    function cambiarNumeros(){
+    	$lista =  $( "#clasificacion li div span" ).toArray();
+		$num = 1;
+		for (var i = 0; i < $lista.length; i++) {			
+			if ((i%2) == 0){
+				$lista[i].innerHTML = $num;
+				$num = $num+1;
+			};
+		};	
+    };
+
 });
