fdd0da477f072b1b81e1c2c53bd829904d7737a9
luisarturo1989@gmail.com <yZ8Eg7ZZ7JH7>
2015-06-10 22:01:34 -0500

    Templates
    Se agregan metodos encargados de generar las paginas

--- generator/GeneratorPoll.rb
+++ generator/GeneratorPoll.rb
@@ -1,6 +1,6 @@
 require "erb"
 require "rexml/document"
-             
+
 ARGV[0] = "../encuestas/xml/encuesta.xml"
 
 def readPollFileXml ( pathFile )
@@ -13,18 +13,47 @@ def readPollFileXml ( pathFile )
     val = 0
     # Se realiza la lectura de cada una las preguntas
     pollChild.each_element("question") do | questionChild |
-      ask = questionChild.elements["ask"].attributes["value"].to_s
-      listOptions = getListOptions(questionChild)
-      print listOptions
-      print "\n\n" 
-    end    
+      type = questionChild.attributes["type"]
+      resultado = case type
+      when "smu_rb"
+        then getsmu_rb(questionChild)
+      when "smu_s"
+        then getsmu_s(questionChild)
+      when "smr_cb"
+        then getsmr_cb(questionChild)
+      when "smr_sm"
+        then getsmr_sm(questionChild)
+      when "mmr_rb"
+        then getmmr_rb(questionChild)
+      when "mmr_cb"
+        then getmmr_cb(questionChild)
+      when "mmr_s"
+        then "mmr_s"
+      when "loc_ul"
+        then getloc_ul(questionChild)
+      when "eru_rb"
+        then geteru_rb(questionChild)
+      when "psu_t"
+        then getpsu_t(questionChild)
+      when "psm_t"
+        then getpsm_t(questionChild)
+      when "psc_ta"
+        then getpsc_ta(questionChild)
+      when "pfh_s"
+        then getpfh_s(questionChild)
+      when "psh_s"
+        then getpsh_s(questionChild)
+      end
+      print "respuesta: #{resultado}\n"
+
+    end
   end
 end
 
 # Método encargado de obtener de un determinado elemento el listado de opciones
-def getListOptions ( element )
+def getListOptions ( element, value )
   listOptions = []
-  element.each_element("option") do |option|
+  element.each_element( value ) do |option|
     has = Hash.new
     option.attributes.each_attribute do |attri|
       has[attri.expanded_name] = attri.value
@@ -32,6 +61,124 @@ def getListOptions ( element )
     listOptions.push(has)
   end
   return listOptions
+end
+
+def getsmu_rb(questionChild)
+  number = questionChild.attributes["number"]
+  required = questionChild.attributes["required"]
+  type = questionChild.attributes["type"]
+  ask = questionChild.elements["ask"].attributes["value"].to_s
+  listOptions = getListOptions(questionChild,"option")
+  return listOptions.to_s
+end
+
+def getsmu_s(questionChild)
+  number = questionChild.attributes["number"]
+  required = questionChild.attributes["required"]
+  type = questionChild.attributes["type"]
+  ask = questionChild.elements["ask"].attributes["value"].to_s
+  listOptions = getListOptions(questionChild,"option")
+  return listOptions.to_s
+end
+
+def getsmr_cb (questionChild)
+  number = questionChild.attributes["number"]
+  required = questionChild.attributes["required"]
+  type = questionChild.attributes["type"]
+  ask = questionChild.elements["ask"].attributes["value"].to_s
+  listOptions = getListOptions(questionChild,"option")
+  return listOptions.to_s
+end
+
+def getsmr_sm (questionChild)
+  number = questionChild.attributes["number"]
+  required = questionChild.attributes["required"]
+  type = questionChild.attributes["type"]
+  ask = questionChild.elements["ask"].attributes["value"].to_s
+  listOptions = getListOptions(questionChild,"option")
+  return listOptions.to_s
+end
+
+def getloc_ul (questionChild)
+  number = questionChild.attributes["number"]
+  required = questionChild.attributes["required"]
+  type = questionChild.attributes["type"]
+  ask = questionChild.elements["ask"].attributes["value"].to_s
+  listOptions = getListOptions(questionChild,"option")
+  return listOptions.to_s
+end
+
+def getpsm_t (questionChild)
+  number = questionChild.attributes["number"]
+  required = questionChild.attributes["required"]
+  type = questionChild.attributes["type"]
+  ask = questionChild.elements["ask"].attributes["value"].to_s
+  listOptions = getListOptions(questionChild,"option")
+  return listOptions.to_s
+end
+
+def getmmr_rb (questionChild)
+  number = questionChild.attributes["number"]
+  required = questionChild.attributes["required"]
+  type = questionChild.attributes["type"]
+  ask = questionChild.elements["ask"].attributes["value"].to_s
+  listRows = getListOptions(questionChild.elements["rows"],"option")
+  listColumns = getListOptions(questionChild.elements["columns"],"option")
+  return "Listas filas: #{listRows} columnas: #{listColumns}"
+end
+
+def getmmr_cb (questionChild)
+  number = questionChild.attributes["number"]
+  required = questionChild.attributes["required"]
+  type = questionChild.attributes["type"]
+  ask = questionChild.elements["ask"].attributes["value"].to_s
+  listRows = getListOptions(questionChild.elements["rows"],"option")
+  listColumns = getListOptions(questionChild.elements["columns"],"option")
+  return "Listas filas: #{listRows} columnas: #{listColumns}"
+end
+
+def geteru_rb (questionChild)
+  number = questionChild.attributes["number"]
+  required = questionChild.attributes["required"]
+  type = questionChild.attributes["type"]
+  ask = questionChild.elements["ask"].attributes["value"].to_s
+  has = Hash.new
+  questionChild.elements["range"].attributes.each_attribute do |range|
+    has[range.expanded_name] = range.value
+  end
+  return "#{has}"
+end
+
+def getpsu_t (questionChild)
+  number = questionChild.attributes["number"]
+  required = questionChild.attributes["required"]
+  type = questionChild.attributes["type"]
+  ask = questionChild.elements["ask"].attributes["value"].to_s
+  return ask
+end
+
+def getpsc_ta (questionChild)
+  number = questionChild.attributes["number"]
+  required = questionChild.attributes["required"]
+  type = questionChild.attributes["type"]
+  ask = questionChild.elements["ask"].attributes["value"].to_s
+  return ask
+end
+
+def getpfh_s (questionChild)
+  number = questionChild.attributes["number"]
+  required = questionChild.attributes["required"]
+  type = questionChild.attributes["type"]
+  ask = questionChild.elements["ask"].attributes["value"].to_s
+  return ask
+end
+
+def getpsh_s (questionChild)
+  number = questionChild.attributes["number"]
+  required = questionChild.attributes["required"]
+  type = questionChild.attributes["type"]
+  ask = questionChild.elements["ask"].attributes["value"].to_s
+  return ask
 end
 
 if ARGV[0]
